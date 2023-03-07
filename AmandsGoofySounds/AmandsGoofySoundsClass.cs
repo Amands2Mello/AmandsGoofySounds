dff45ff8791f08d4cfc8d84f1e1dd849b1ac055c
Amands2Mello <112194796+Amands2Mello@users.noreply.github.com>
2023-03-07 15:31:03 -0300

    Add files via upload

---
+++ AmandsGoofySounds/AmandsGoofySoundsClass.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using EFT;
+using Comfort.Common;
+
+namespace AmandsGoofySounds
+{
+    public class AmandsGoofySoundsClass : MonoBehaviour
+    {
+        public static LocalPlayer localPlayer;
+        private static List<AudioClip> SoundRandom = new List<AudioClip>();
+        private static List<AudioClip> SoundHit = new List<AudioClip>();
+        private static List<AudioClip> SoundDeath = new List<AudioClip>();
+        private static List<AudioClip> SoundSpotted = new List<AudioClip>();
+        private static Dictionary<string, float> Playing = new Dictionary<string, float>();
+        public void Start()
+        {
+            ReloadFiles();
+        }
+        public void Update()
+        {
+        }
+        public async void PlaySoundRandom()
+        {
+            await Task.Delay((int)(UnityEngine.Random.Range(AmandsGoofySoundsPlugin.MinRandom.Value, AmandsGoofySoundsPlugin.MaxRandom.Value) * 1000));
+            if (localPlayer != null)
+            {
+                List<Player> SoundPlayers = new List<Player>();
+                foreach (Player player in Singleton<GameWorld>.Instance.RegisteredPlayers)
+                {
+                    if (player == localPlayer) continue;
+                    if (Vector3.Distance(localPlayer.Position, player.Position) < AmandsGoofySoundsPlugin.Distance.Value) SoundPlayers.Add(player);
+                }
+                if (SoundPlayers.Count != 0 && (UnityEngine.Random.Range(0.0f, 0.99f) < AmandsGoofySoundsPlugin.RandomChance.Value))
+                {
+                    System.Random rnd = new System.Random();
+                    PlayAmandsGoofySounds(SoundPlayers[rnd.Next(SoundPlayers.Count)], ESoundType.Random);
+                }
+                PlaySoundRandom();
+            }
+        }
+        public void PlayAmandsGoofySounds(Player player, ESoundType soundType)
+        {
+            if (player != null && localPlayer != null)
+            {
+                if (Vector3.Distance(localPlayer.Position, player.Position) > AmandsGoofySoundsPlugin.Distance.Value) return;
+                if (Playing.ContainsKey(player.ProfileId))
+                {
+                    if (Playing[player.ProfileId] < Time.time)
+                    {
+                        Playing.Remove(player.ProfileId);
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                if (AmandsGoofySoundsPlugin.EnableSounds.Value && player != localPlayer)
+                {
+                    System.Random rnd = new System.Random();
+                    AudioClip audioClip = null;
+                    switch (soundType)
+                    {
+                        case ESoundType.Random:
+                            audioClip = SoundRandom[rnd.Next(SoundRandom.Count)];
+                            break;
+                        case ESoundType.Hit:
+                            audioClip = SoundHit[rnd.Next(SoundHit.Count)];
+                            break;
+                        case ESoundType.Spotted:
+                            audioClip = SoundSpotted[rnd.Next(SoundSpotted.Count)];
+                            break;
+                    }
+                    if (audioClip != null)
+                    {
+                        Singleton<BetterAudio>.Instance.PlayAtPoint(player.Position, audioClip, AmandsGoofySoundsPlugin.Distance.Value, BetterAudio.AudioSourceGroupType.Character, AmandsGoofySoundsPlugin.Rolloff.Value, AmandsGoofySoundsPlugin.Volume.Value, EOcclusionTest.Regular).transform.SetParent(player.Transform.Original);
+                        Playing.Add(player.ProfileId, Time.time + audioClip.length);
+                    }
+                }
+            }
+        }
+        public void PlaySoundDeath(Vector3 position)
+        {
+            if (AmandsGoofySoundsPlugin.EnableSounds.Value && localPlayer != null)
+            {
+                if (Vector3.Distance(localPlayer.Position, position) > AmandsGoofySoundsPlugin.Distance.Value) return;
+                System.Random rnd = new System.Random();
+                AudioClip audioClip = SoundDeath[rnd.Next(SoundDeath.Count)];
+                if (audioClip != null)
+                {
+                    Singleton<BetterAudio>.Instance.PlayAtPoint(position, audioClip, AmandsGoofySoundsPlugin.Distance.Value, BetterAudio.AudioSourceGroupType.Character, AmandsGoofySoundsPlugin.Rolloff.Value, AmandsGoofySoundsPlugin.Volume.Value, EOcclusionTest.Regular);
+                }
+            }
+        }
+        public void ReloadFiles()
+        {
+            string[] AudioFiles = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "/BepInEx/plugins/Sound/Random/");
+            foreach (string File in AudioFiles)
+            {
+                LoadAudioClip(File, ESoundType.Random);
+            }
+            AudioFiles = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "/BepInEx/plugins/Sound/Hit/");
+            foreach (string File in AudioFiles)
+            {
+                LoadAudioClip(File, ESoundType.Hit);
+            }
+            AudioFiles = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "/BepInEx/plugins/Sound/Death/");
+            foreach (string File in AudioFiles)
+            {
+                LoadAudioClip(File, ESoundType.Death);
+            }
+            AudioFiles = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "/BepInEx/plugins/Sound/Spotted/");
+            foreach (string File in AudioFiles)
+            {
+                LoadAudioClip(File, ESoundType.Spotted);
+            }
+        }
+        async void LoadAudioClip(string path, ESoundType soundType)
+        {
+            AudioClip audioClip = await RequestAudioClip(path);
+            switch (soundType)
+            {
+                case ESoundType.Random:
+                    SoundRandom.Add(audioClip);
+                    break;
+                case ESoundType.Hit:
+                    SoundHit.Add(audioClip);
+                    break;
+                case ESoundType.Death:
+                    SoundDeath.Add(audioClip);
+                    break;
+                case ESoundType.Spotted:
+                    SoundSpotted.Add(audioClip);
+                    break;
+            }
+        }
+        async Task<AudioClip> RequestAudioClip(string path)
+        {
+            string extension = Path.GetExtension(path);
+            AudioType audioType = AudioType.WAV;
+            switch (extension)
+            {
+                case ".wav":
+                    audioType = AudioType.WAV;
+                    break;
+                case ".ogg":
+                    audioType = AudioType.OGGVORBIS;
+                    break;
+            }
+            UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(path, audioType);
+            var SendWeb = www.SendWebRequest();
+
+            while (!SendWeb.isDone)
+                await Task.Yield();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                return null;
+            }
+            else
+            {
+                AudioClip audioclip = DownloadHandlerAudioClip.GetContent(www);
+                return audioclip;
+            }
+        }
+    }
+    public enum ESoundType
+    {
+        Random,
+        Hit,
+        Death,
+        Spotted
+    }
+}
---
+++ AmandsGoofySounds/Program.cs
@@ -0,0 +1,122 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using EFT;
+using System.Reflection;
+using Aki.Reflection.Patching;
+using UnityEngine;
+using System.Threading.Tasks;
+
+namespace AmandsGoofySounds
+{
+    [BepInPlugin("com.Amanda.GoofySounds", "GoofySounds", "1.0.0")]
+    public class AmandsGoofySoundsPlugin : BaseUnityPlugin
+    {
+        public static GameObject Hook;
+        public static AmandsGoofySoundsClass AmandsGoofySoundsClassComponent;
+        public static ConfigEntry<bool> EnableSounds { get; set; }
+        public static ConfigEntry<float> Distance { get; set; }
+        public static ConfigEntry<int> Rolloff { get; set; }
+        public static ConfigEntry<float> Volume { get; set; }
+        public static ConfigEntry<float> RandomChance { get; set; }
+        public static ConfigEntry<float> MinRandom { get; set; }
+        public static ConfigEntry<float> MaxRandom { get; set; }
+        public static ConfigEntry<float> HitChance { get; set; }
+        public static ConfigEntry<float> DeathChance { get; set; }
+        public static ConfigEntry<float> SpottedChance { get; set; }
+        public static ConfigEntry<float> SpottedCooldown { get; set; }
+        private void Awake()
+        {
+            Hook = new GameObject();
+            AmandsGoofySoundsClassComponent = Hook.AddComponent<AmandsGoofySoundsClass>();
+            DontDestroyOnLoad(Hook);
+        }
+        private void Start()
+        {
+            EnableSounds = Config.Bind("AmandsGoofySounds", "EnableSounds", true, new ConfigDescription("Supported Files WAV OGG", null, new ConfigurationManagerAttributes { Order = 210 }));
+            Distance = Config.Bind("AmandsGoofySounds", "Distance", 99f, new ConfigDescription("", null, new ConfigurationManagerAttributes { Order = 200 }));
+            Rolloff = Config.Bind("AmandsGoofySounds", "Rolloff", 100, new ConfigDescription("", null, new ConfigurationManagerAttributes { Order = 190 }));
+            Volume = Config.Bind("AmandsGoofySounds", "Volume", 1.0f, new ConfigDescription("", new AcceptableValueRange<float>(0.01f, 4f), new ConfigurationManagerAttributes { Order = 180 }));
+            RandomChance = Config.Bind("AmandsGoofySounds", "RandomChance", 0.69f, new ConfigDescription("", new AcceptableValueRange<float>(0.0f, 1f), new ConfigurationManagerAttributes { Order = 170 }));
+            MinRandom = Config.Bind("AmandsGoofySounds", "MinRandomTimer", 10f, new ConfigDescription("", null, new ConfigurationManagerAttributes { Order = 160 }));
+            MaxRandom = Config.Bind("AmandsGoofySounds", "MaxRandomTimer", 60f, new ConfigDescription("", null, new ConfigurationManagerAttributes { Order = 150 }));
+            HitChance = Config.Bind("AmandsGoofySounds", "HitChance", 0.69f, new ConfigDescription("", new AcceptableValueRange<float>(0.0f, 1f), new ConfigurationManagerAttributes { Order = 140 }));
+            DeathChance = Config.Bind("AmandsGoofySounds", "DeathChance", 0.69f, new ConfigDescription("", new AcceptableValueRange<float>(0.0f, 1f), new ConfigurationManagerAttributes { Order = 130 }));
+            SpottedChance = Config.Bind("AmandsGoofySounds", "SpottedChance", 0.69f, new ConfigDescription("", new AcceptableValueRange<float>(0.0f, 1f), new ConfigurationManagerAttributes { Order = 120 }));
+            SpottedCooldown = Config.Bind("AmandsGoofySounds", "SpottedCooldown", 30.0f, new ConfigDescription("", null, new ConfigurationManagerAttributes { Order = 110 }));
+
+            new AmandsLocalPlayerPatch().Enable();
+            new AmandsGoofySoundsKillPatch().Enable();
+            new AmandsGoofySoundsDamagePatch().Enable();
+            new AmandsGoofySoundsGoalPatch().Enable();
+        }
+    }
+    public class AmandsLocalPlayerPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(LocalPlayer).GetMethod("Create", BindingFlags.Static | BindingFlags.Public);
+        }
+        [PatchPostfix]
+        private static void PatchPostFix(ref Task<LocalPlayer> __result)
+        {
+            LocalPlayer localPlayer = __result.Result;
+            if (localPlayer != null && localPlayer.IsYourPlayer)
+            {
+                AmandsGoofySoundsClass.localPlayer = localPlayer;
+                AmandsGoofySoundsPlugin.AmandsGoofySoundsClassComponent.PlaySoundRandom();
+            }
+        }
+    }
+    public class AmandsGoofySoundsKillPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(Player).GetMethod("OnBeenKilledByAggressor", BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+        [PatchPostfix]
+        private static void PatchPostFix(ref Player __instance, Player aggressor, DamageInfo damageInfo, EBodyPart bodyPart, EDamageType lethalDamageType)
+        {
+            if (UnityEngine.Random.Range(0.0f, 0.99f) < AmandsGoofySoundsPlugin.DeathChance.Value) AmandsGoofySoundsPlugin.AmandsGoofySoundsClassComponent.PlaySoundDeath(damageInfo.HitPoint);
+        }
+    }
+    public class AmandsGoofySoundsDamagePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(Player).GetMethod("ApplyDamageInfo", BindingFlags.Instance | BindingFlags.Public);
+        }
+        [PatchPostfix]
+        private static void PatchPostFix(ref Player __instance, DamageInfo damageInfo, EBodyPart bodyPartType)
+        {
+            if (UnityEngine.Random.Range(0.0f, 0.99f) < AmandsGoofySoundsPlugin.HitChance.Value) AmandsGoofySoundsPlugin.AmandsGoofySoundsClassComponent.PlayAmandsGoofySounds(__instance, ESoundType.Hit);
+        }
+    }
+    public class AmandsGoofySoundsGoalPatch : ModulePatch
+    {
+        public static float TimeTest = 0;
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(BotGroupClass).GetMethod("CalcGoalForBot");
+        }
+        [PatchPostfix]
+        public static void PatchPostfix(BotOwner bot)
+        {
+            object goalEnemy = bot.Memory.GetType().GetProperty("GoalEnemy").GetValue(bot.Memory);
+
+            if (goalEnemy != null)
+            {
+                IAIDetails person = (IAIDetails)goalEnemy.GetType().GetProperty("Person").GetValue(goalEnemy);
+                bool isVisible = (bool)goalEnemy.GetType().GetProperty("IsVisible").GetValue(goalEnemy);
+
+                if (person.GetPlayer.IsYourPlayer && isVisible)
+                {
+                    if (TimeTest < Time.time)
+                    {
+                        TimeTest = Time.time + AmandsGoofySoundsPlugin.SpottedCooldown.Value;
+                        if (UnityEngine.Random.Range(0.0f, 0.99f) < AmandsGoofySoundsPlugin.SpottedChance.Value) AmandsGoofySoundsPlugin.AmandsGoofySoundsClassComponent.PlayAmandsGoofySounds(bot.GetPlayer, ESoundType.Spotted);
+                    }
+                }
+            }
+        }
+    }
+}
